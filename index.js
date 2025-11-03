739670f3242b5e699fb0b1458178a0fd9f62c471
Aafrin Shahanas <164051727+aafrinshahas@users.noreply.github.com>
2025-11-03 13:15:19 +0530

    CROS UPDATE

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ const PORT = process.env.PORT;
 //Middleware
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: ["https://chat-app-beryl-three.vercel.app"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 //Database
 connectDatabase();
